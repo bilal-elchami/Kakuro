c385027317fa7355628377dbbc1656c7ecc7ca93
Elie Daher <elieahd@hotmail.com>
2018-02-17 13:01:19 +0100

    initializing grid
    created a grid class with several method implemented.

--- kakuro/Kakuro.cpp
+++ kakuro/Kakuro.cpp
@@ -1,15 +1,54 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void initGrid() {
-	int lines;
-	int columns;
-	cin >> lines;
-	cout << "";
-}
+class Grid {
+  private:
+	int numColumns;
+	int numRows;
+	int colValues[10];
+	int rowValues[10];
+	void substringValues(string line, string delimiter, int lineCount) {
+		size_t pos = 0;
+		while ((pos = line.find(delimiter)) && (line.length() > 0)) {
+			int value = atoi(line.substr(0, pos).c_str());
+			if (lineCount == 1) { // row
+				rowValues[numRows] = value;
+				numRows += 1;
+			} else if (lineCount == 2) { // column
+				colValues[numColumns] = value;
+				numColumns += 1;
+			}
+			line.erase(0, pos + delimiter.length());
+		}
+	}
+
+  public:
+	void initialize(string path, string delimiter) {
+		ifstream file(path);
+		string line;
+		numRows = 0;
+		numColumns = 0;
+		int lineCount = 0;
+		while (getline(file, line)) {
+			lineCount++;
+			substringValues(line, delimiter, lineCount);
+		}
+	}
+	void displayInfo() {
+		cout << " cols " << numColumns << endl;
+		for (int i = 0; i < numColumns; i++) {
+			cout << "col [" << (i + 1) << "] " << colValues[i] << endl;
+		}
+		cout << endl << "rows " << numRows << endl;
+		for (int i = 0; i < numRows; i++) {
+			cout << "row [" << (i + 1) << "] " << rowValues[i] << endl;
+		}
+	}
+};
 
 int main() {
-	int lines = 4;
-	int columns = 4;
-	int possibleValues[] = {1, 2, 3, 4};
+	Grid grid;
+	grid.initialize("C:\\Users\\eliea\\Desktop\\grid.txt", ";");
+	grid.displayInfo();
 }
