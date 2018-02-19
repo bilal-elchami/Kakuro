af125819e44cf92a6430d69cb2577129eaedcc54
Elie Daher <elieahd@hotmail.com>
2018-02-19 11:45:12 +0100

    added random value generator in class

--- kakuro/Grid.cpp
+++ kakuro/Grid.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <fstream>
 #include "Grid.h"
+using namespace std;
 
-Grid::Grid() {
-
+Grid::Grid(string path, string delimiter) {
+    ifstream file(path);
+    string line;
+    int lineCount = 0;
+    while (getline(file, line)) {
+        lineCount++;
+        substringValues(line, delimiter, lineCount);
+    }
 }
 
-Grid::~Grid() {
+void Grid::displayInfo() {
+    cout << " cols " << numColumns << endl;
+    for (int i = 0; i < numColumns; i++) {
+        cout << "col [" << (i + 1) << "] " << colValues[i] << endl;
+    }
+    cout << endl << "rows " << numRows << endl;
+    for (int i = 0; i < numRows; i++) {
+        cout << "row [" << (i + 1) << "] " << rowValues[i] << endl;
+    }
+}
 
+void Grid::substringValues(string line, string delimiter, int lineCount) {
+    size_t pos = 0;
+    while ((pos = line.find(delimiter)) && (line.length() > 0)) {
+        int value = atoi(line.substr(0, pos).c_str());
+        if (lineCount == 1) { // row
+            rowValues[numRows] = value;
+            numRows += 1;
+        } else if (lineCount == 2) { // column
+            colValues[numColumns] = value;
+            numColumns += 1;
+        }
+        line.erase(0, pos + delimiter.length());
+    }
 }
